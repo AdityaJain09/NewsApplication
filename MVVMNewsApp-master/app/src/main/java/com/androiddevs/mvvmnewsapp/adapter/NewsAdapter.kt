bbc51125c7622b69933027ca3e7b0ae3af9a4693
Aditya <adityaj519@gmail.com>
2020-11-24 00:17:08 +0530

    added reycleview adapter

---
+++ MVVMNewsApp-master/app/src/main/java/com/androiddevs/mvvmnewsapp/adapter/NewsAdapter.kt
@@ -0,0 +1,44 @@
+package com.androiddevs.mvvmnewsapp.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.androiddevs.mvvmnewsapp.R
+import com.androiddevs.mvvmnewsapp.models.Article
+
+
+
+class NewsAdapter : RecyclerView.Adapter<NewsAdapter.NewsViewHolder>(){
+
+    private val diffUtilObj = object  : DiffUtil.ItemCallback<Article>(){
+        override fun areItemsTheSame(oldItem: Article, newItem: Article): Boolean {
+            oldItem.url == newItem.url
+        }
+
+        override fun areContentsTheSame(oldItem: Article, newItem: Article): Boolean {
+            oldItem == newItem
+        }
+
+    }
+
+    val data = listOf<Article>()
+    inner class  NewsViewHolder(view : View) : RecyclerView.ViewHolder(view){
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsViewHolder {
+        return NewsViewHolder(LayoutInflater.from(parent.context).inflate(
+            R.layout.item_article_preview,
+            parent,
+            false
+        ))
+    }
+
+    override fun onBindViewHolder(holder: NewsViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int = data.size
+}
